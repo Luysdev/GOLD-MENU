4335349be476cce1ca8c8b05fbd3323fe6d4f68b
Luís Eduardo da Silva Batista <119604941+Luysdev@users.noreply.github.com>
2024-09-22 14:43:47 -0300

    .

--- BACKEND/bd/api/index.js
+++ BACKEND/bd/api/index.js
@@ -1,11 +1,16 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import { Mesa } from "./Entidades/crud_mesa";
 
 export const app = () => {
     const expressApp = express();
     expressApp.use(express.json());
     expressApp.use(cors());
-    expressApp.listen(3333);
-    return expressApp;
-} 
+    expressApp.listen(3333, () => {
+        console.log("Server is running on http://localhost:3333");
+    });
 
+    return expressApp;
+};
+
+Mesa(app);
